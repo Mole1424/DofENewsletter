0717f8c7d7a1a0b2806a73994fc5f93e6a2aea30
Joel Coulon <Joel.Coulon@warwick.ac.uk>
2024-09-13 18:42:18 +0100

    added cookies for urls

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return render_template("main.html")
+    links = request.cookies.get("links")
+    if links is not None:
+        links = links.split(",")
+    else:
+        links = [""] * 5
+    return render_template("main.html", links=links)
