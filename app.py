d9202be4b1d46a2073a43c85eaf8654169496067
Joel Coulon <Joel.Coulon@warwick.ac.uk>
2024-09-16 14:22:06 +0200

    markdown support

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from flask import Flask, render_template, request
+from markdown import markdown
+from markdown.inlinepatterns import InlineProcessor
+from markdown.extensions import Extension
+from markupsafe import escape
+import xml.etree.ElementTree as etree
 
 app = Flask(__name__)
 
@@ -11,12 +16,30 @@ def index():
     return render_template("main.html", num_events=int(num_events))
 
 
+# shamelessly stolen from docs (https://python-markdown.github.io/extensions/api/#example_3)
+class DelInlineProcessor(InlineProcessor):
+    def handleMatch(self, m, data):
+        el = etree.Element("del")
+        el.text = m.group(1)
+        return el, m.start(0), m.end(0)
+
+
+class DelExtension(Extension):
+    def extendMarkdown(self, md):
+        DEL_PATTERN = r"~~(.*?)~~"  # like ~~del~~
+        md.inlinePatterns.register(DelInlineProcessor(DEL_PATTERN, md), "del", 175)
+
+
 @app.route("/createEmail", methods=["POST"])
 def create_email():
     json = request.get_json()
     week_no = json["weekNo"]
     introduction = json["introduction"]
     events = json["events"]
+    for event in events:
+        event["description"] = markdown(
+            escape(event["description"]), extensions=[DelExtension()]
+        )
     conclusion = json["conclusion"]
     name = json["name"]
     whatsapp = json["whatsapp"]
